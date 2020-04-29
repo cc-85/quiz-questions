d94910b52c8667feec3444b29569fa7a5f09bc44
Caoimhe Clarke <caoimhepower@gmail.com>
2020-04-29 12:39:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="app.js" charset="utf-8"></script>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+      <h1>Quiz!</h1>
+      <?// include('widget.php') ?>
+   </body>
+</html>
+
+
+
